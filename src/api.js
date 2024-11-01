b8b64a95d6b515df037d000f031b133369b5eb80
Priyam Shah <priyam10302@gmail.com>
2024-11-01 16:21:43 -0400

    built city search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'ea62e73f9dmsh388dabd99427996p1d75bfjsn98efb770a530',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
