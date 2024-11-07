ccf78d0286b1430a7ed53cc1bab969f82c44a4c5
Priyam Shah <108848788+priyam-02@users.noreply.github.com>
2024-11-07 11:54:07 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ea62e73f9dmsh388dabd99427996p1d75bfjsn98efb770a530',
+		'x-rapidapi-key': 'your_api_key',
 		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
 
-export const WEATHER_API_KEY = '1a1e02ca517631739319337fb71d8507'
+export const WEATHER_API_KEY = 'your_api_key'
